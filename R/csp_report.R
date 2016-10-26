402a53190849042c2ed6fd4cd86d85e8ae5bcb90
Luke Zachmann <luke@csp-inc.org>
2016-10-26 11:17:44 -0700

    tweaks to the forked default html template to, i.e., to include a pretty abstract

--- R/csp_report.R
+++ R/csp_report.R
@@ -1,16 +1,25 @@
 #' Convert to an HTML CSP Report.
 #'
 #' A CSP Report is a lightweight webpage suitable for sharing.
-#' @param toc Generate a Table of Contents? Defaults to TRUE.
-#' @param theme Use a theme? Defaults to flatly.
+#' @param toc Generate a Table of Contents? Defaults to \code{true}.
+#' @param theme Visual theme. Defaults to \code{flatly}. Other good options may
+#' include \code{journal}, \code{united}, \code{sandstone}, or \code{yeti}.
+#' @param highlight Syntax highlighting style. Defaults to \code{tango}. Pass
+#' \code{NULL} to prevent syntax highlighting.
+#' @param smart Produce typographically correct output, converting straightquotes
+#' to curly quotes, --- to em-dashes, -- to en-dashes, and ... to ellipses?
+#' Defaults to \code{true}.
+#' @param template Pandoc template to use for rendering. Defaults to the
+#' rmarkdown package default template. Alternatively, pass \code{html_csp} to
+#' use the rmdy package template .
 #' @export
 
-csp_report <- function(toc = TRUE, theme = "flatly", template = "default",
-                       ...) {
+csp_report <- function(toc = TRUE, smart = TRUE, theme = "flatly",
+                       highlight = "tango", template = "default", ...) {
 
   # get the locations of resource files located within the package
   resources_dir <- "rmarkdown/templates/csp_report/skeleton"
-  if (template=="custom")
+  if (template=="html_csp")
     template <- system.file(file.path(resources_dir, "default_fork.html"),
                             package = "rmdy")
   css <-
@@ -22,7 +31,9 @@ csp_report <- function(toc = TRUE, theme = "flatly", template = "default",
 
   # call the base html_document function
   rmarkdown::html_document(toc = toc,
+                           smart = TRUE,
                            theme = theme,
+                           highlight = highlight,
                            template = template,
                            css = css,
                            includes = rmarkdown::includes(in_header = header,
--- README.md
+++ README.md
@@ -12,13 +12,21 @@ devtools::install_github("csp-inc/rmdy")
 
 #### Output Formats
 
-In the RMarkdown metadata section
+An example of the RMarkdown metadata section:
 
 ``` yaml
 ---
-title: "Sample Document"
+title: "Four Forest Restoration Initiative - Landscape Pattern Analysis"
+author: "Luke J. Zachmann and Brett G. Dickson"
+date: "`r gsub(' 0', ' ', format(Sys.time(), '%B %d, %Y'))`"
+abstract: |
+    A summary of data products produced for the US Forest Service, including
+    links to the data, metadata regarding the accuracy and precision of specific
+    results, and guidance for appropriate uses of the data.
+bibliography: bibliography.bib
+csl: conservation-biology.csl
 output:
   rmdy::csp_report:
-    toc: true
+    template: html_csp
 ---
 ```
